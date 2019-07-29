3315980d02ad984610812ff36c683069c3bec38f
anastasia <an.yakimchuk@gmail.com>
2019-07-29 21:43:18 +0200

    Add transliteration, Date.humanizeDiff

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -2,6 +2,8 @@ package ru.skillbranch.devintensive.extensions
 
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.absoluteValue
+import kotlin.math.roundToInt
 
 const val SECOND = 1000L
 const val MINUTE = 60 * SECOND
@@ -28,6 +30,87 @@ fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
     this.time = time
 
     return this
+}
+
+fun Date.humanizeDiff(): String {
+    val currentDate = Date()
+    val diff = currentDate.time - this.time
+    val absVal = diff.absoluteValue
+
+    return when {
+        absVal / SECOND in 0..1 -> return "только что"
+        absVal / SECOND in 1..45 -> return "${if (diff > 0) "несколько секунд назад" else "через несколько секунд"}"
+        absVal / SECOND in 45..75 -> return "${if (diff > 0) "минуту назад" else "через минуту"}"
+        absVal / SECOND in 75..45 * MINUTE / SECOND -> return "${if (diff < 0) "через " else ""}${getCorrectPhrase(
+            convertFromMs(
+                absVal,
+                TimeUnits.MINUTE
+            ), TimeUnits.MINUTE
+        )}${if (diff > 0) " назад" else ""}"
+        absVal / MINUTE in 45..75 -> return "${if (diff > 0) "час назад" else "через час"}"
+        absVal / MINUTE in 75..22 * HOUR / MINUTE -> return "${if (diff < 0) "через " else ""}${getCorrectPhrase(
+            convertFromMs(
+                absVal,
+                TimeUnits.HOUR
+            ), TimeUnits.HOUR
+        )}${if (diff > 0) " назад" else ""}"
+        absVal / HOUR in 22..26 -> return "${if (diff > 0) "день назад" else "через день"}"
+        absVal / HOUR in 26..360 * DAY / HOUR -> return "${if (diff < 0) "через " else ""}${getCorrectPhrase(
+            convertFromMs(
+                absVal,
+                TimeUnits.DAY
+            ), TimeUnits.DAY
+        )}${if (diff > 0) " назад" else ""}"
+        absVal / DAY > 360 -> return "${if (diff > 0) "более года назад" else "более чем через год"}"
+        else -> "Ошибка"
+    }
+}
+
+private fun convertFromMs(number: Long, type: TimeUnits): Int {
+    return when (type) {
+        TimeUnits.SECOND -> (number * 1.0 / SECOND).roundToInt()
+        TimeUnits.MINUTE -> (number * 1.0 / MINUTE).roundToInt()
+        TimeUnits.HOUR -> (number * 1.0 / HOUR).roundToInt()
+        TimeUnits.DAY -> (number * 1.0 / DAY).roundToInt()
+    }
+}
+
+private fun getCorrectPhrase(number: Int, type: TimeUnits): String {
+    val rem: Int
+
+    if (number > 20) {
+        rem = number % 10
+    } else {
+        rem = number % 20
+    }
+
+    return when (type) {
+        TimeUnits.MINUTE -> {
+            when (rem) {
+                0, in 5..20 -> return "$number минут"
+                1 -> return "$number минута"
+                in 2..4 -> return "$number минуты"
+                else -> ""
+            }
+        }
+        TimeUnits.HOUR -> {
+            when (rem) {
+                0, in 5..20 -> return "$number часов"
+                1 -> return "$number час"
+                in 2..4 -> return "$number часа"
+                else -> ""
+            }
+        }
+        TimeUnits.DAY -> {
+            when (rem) {
+                0, in 5..20 -> return "$number дней"
+                1 -> return "$number день"
+                in 2..4 -> return "$number дня"
+                else -> ""
+            }
+        }
+        else -> ""
+    }
 }
 
 enum class TimeUnits {
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,5 +1,8 @@
 package ru.skillbranch.devintensive.utils
 
+import java.io.*
+import kotlin.collections.HashMap
+
 object Utils {
     fun parseFullName(fullName: String?): Pair<String?, String?> {
 
@@ -9,6 +12,72 @@ object Utils {
         val lastName = checkName(parts?.getOrNull(1))
 
         return firstName to lastName
+    }
+
+    fun toInitials(firstName: String?, lastName: String?): String? {
+        val clearedFirstName = firstName?.trim()
+        val clearedLastName = lastName?.trim()
+
+        return when {
+            !clearedFirstName.isNullOrEmpty() && !clearedLastName.isNullOrEmpty() ->
+                "${clearedFirstName.first().toUpperCase()}${clearedLastName.first().toUpperCase()}"
+            clearedFirstName.isNullOrEmpty() && !clearedLastName.isNullOrEmpty() ->
+                "${clearedLastName.first().toUpperCase()}"
+            !clearedFirstName.isNullOrEmpty() && clearedLastName.isNullOrEmpty() ->
+                "${clearedFirstName.first().toUpperCase()}"
+            else -> null
+        }
+    }
+
+    fun transliteration(payload: String, divider: String = " "): String {
+        val transliterationList: HashMap<String, String> = getTransliterationList()
+        val parts = payload.split(" ")
+        val firstName = convertFromCyrillicToLatin(parts[0], transliterationList)
+        val lastName = convertFromCyrillicToLatin(parts[1], transliterationList)
+
+        return "$firstName$divider$lastName"
+    }
+
+    private fun convertFromCyrillicToLatin(word: String, transliterationList: HashMap<String, String>): String {
+        var convertedWord = ""
+
+        for (item in word) {
+            if (!isLatinAlphabet(item)) {
+                val isCapitalized = item.isUpperCase()
+                val latin = transliterationList[item.toString().toLowerCase()]
+
+                convertedWord += "${if (isCapitalized) latin?.capitalize() else latin}"
+            } else {
+                convertedWord += item
+            }
+        }
+
+        return convertedWord
+    }
+
+    private fun isLatinAlphabet(c: Char): Boolean {
+        return when (c) {
+            in 'a'..'z', in 'A'..'Z' -> true
+            else -> false
+        }
+    }
+
+    private fun getTransliterationList(): HashMap<String, String> {
+        val file = File("src/html/transliteration.html")
+        val lines: List<String> = file.readLines()
+        val map = HashMap<String, String>()
+        for (line in lines) {
+            val cleanedLine = line.replace("[,]|[ ]|[\"]".toRegex(), "")
+            val parts = cleanedLine.split(":").toTypedArray()
+
+            if (parts.size >= 2) {
+                val key = parts[0]
+                val value = parts[1]
+                map[key] = value
+            }
+        }
+
+        return map
     }
 
     private fun checkName(name: String?): String? {
